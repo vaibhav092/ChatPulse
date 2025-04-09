6e7eb54e1f80672e0ef4c841b8fabfd684af7dd9
Vaibhav092 <vaibhavcr7092@gmail.com>
2025-04-09 16:04:34 +0530

    added Controllers

--- Backend/Controller/user.controller.js
+++ Backend/Controller/user.controller.js
@@ -1,8 +1,21 @@
 import {asyncHandler} from "../Util/asyncHandler.js"
 import {User} from "../Models/user.Model.js"
 import fileUpload from "../Util/cloudinary.js"
+import jwt from "jsonwebtoken"
 
+const generateAccessAndRefreshToken=async(userId)=>{
+    try {
+        const user= await User.findById(userId)
+        const accessToken=await user.generateRefreshToken();
+        const refreshToken=await user.generateAccessToken();
+        user.refreshToken=refreshToken;
+        user.save({validateBeforeSave:false});
 
+        return {accessToken,refreshToken};
+    } catch (error) {
+        throw new ApiError(500,"Error while generating token")
+    }
+}
 const registerUser=asyncHandler(async (req,res) => {
     const {username,email,password}=req.body
 
@@ -40,14 +53,65 @@ const loginUser=asyncHandler(async (req,res) => {
     if ([username,password].some((x)=>x?.trim()==="")) {
         throw new Error("All fields are required ")
     }
-    const existUser=User.findOne({
-        $or:{username}
+    const user=await User.findOne({
+        $or:[{username}]
     })
-    if(!existUser){
+    if(!user){
         throw new Error("No User Exist")
     }
+    const passwordValid=await existUser.isPasswordCorrect(password)
+
+    if(!passwordValid){
+        throw new Error("Password incorrect")
+    }
+    const {accessToken,refreshToken}=await generateAccessAndRefreshToken(user._id);
+
+    const logedinUser=await User.findById(user._id).select("-password -refreshToken")
+
+    const options={
+        httpOnly:true,
+        secure:true,}
+
+        return res
+        .status(200)
+        .cookie("accessToken",accessToken,options)
+        .cookie("refreshToken",refreshToken,options)
+        .json(new ApiRes(200,{user:logedinUser,accessToken,refreshToken},"Login successfull"))
+
+
+})
+
+const RefreshAcesstoken=asyncHandler(async (req,res) => {
+    const refreshToken=req.cookies.refreshToken;
+
+    if(!refreshToken){
+        throw new Error("No refersh token")
+    }
+    const decoded=jwt.verify(refreshToken, process.env.JWT_SECRET)
+    
+    if(!decoded){
+        throw new Error("refresh Token Error")
+    }
+
+    const user = await User.findById(decoded.userId);
+    if(!user){
+        throw new Error("User not found /TOken mismatch")
+    }
+    const {accessToken}=await generateAccessAndRefreshToken(user._id);
+
+    const options={
+        httpOnly:true,
+        secure:true,}
+
+    return res
+    .status(200)
+    .cookie("accessToken",accessToken,options)
+    .json({
+        message:"Token refreshed"
+    })
+
 })
 
 export {
-    registerUser
+    registerUser,loginUser,RefreshAcesstoken
 }
